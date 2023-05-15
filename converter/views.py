08038ed228360d4efeeda33dac48db7063a07832
Vlad <cacenschivlad@gmail.com>
2023-05-15 08:44:31 +0300

    trying to solve conflicts

--- converter/views.py
+++ converter/views.py
@@ -6,8 +6,44 @@ from functions.functions import *
 from .models import FileModel
 import datetime
 
+#Third-Party
+from utils.hash_utils.hash_utils import SecureHasher
+import sqlite3
+
+#Variables
+DATABASE_PATH = settings.DATABASES['default']['NAME']
+
 def iesiri(request):
     form = FormIesiri()
+
+    url = request.build_absolute_uri()
+    
+    try:
+        if("?token=" in url):
+            sqliteConnection = sqlite3.connect(DATABASE_PATH)
+            cursor = sqliteConnection.cursor()
+            
+            url_token = request.GET.get("token")
+            token = url_token
+            
+            if ("=" in token):
+                return render(request, "403.html", status=403)
+            
+            chiper = SecureHasher.AESCipher()
+            dec_token = chiper.decrypt(url_token)
+            token = dec_token
+            
+            valid = cursor.execute("SELECT * FROM main_user WHERE display_name = ?", [token])
+            sqliteConnection.commit()
+            valid = cursor.fetchall()
+            
+            if(len(valid) == 0):
+                return render(request, "403.html", status = 403)
+            
+    except Exception as e:
+            print(e)
+            return render(request, "403.html", status = 403)
+
     if request.method == 'POST':
         # tip: [0, 1] -> 0 intrari, 1 iesiri
         form = FormIesiri(request.POST, request.FILES)
@@ -34,6 +70,34 @@ def iesiri(request):
 def mapping(request, file):
     path = f'input files/{file}.xlsx'
     model = FileModel.objects.get(nume=f'{file}.xlsx')
+
+    url = request.build_absolute_uri()
+    
+    try:
+        if("?token=" in url):
+            sqliteConnection = sqlite3.connect(DATABASE_PATH)
+            cursor = sqliteConnection.cursor()
+            
+            url_token = request.GET.get("token")
+            token = url_token
+            
+            if ("=" in token):
+                return render(request, "403.html", status=403)
+            
+            chiper = SecureHasher.AESCipher()
+            dec_token = chiper.decrypt(url_token)
+            token = dec_token
+            
+            valid = cursor.execute("SELECT * FROM main_user WHERE display_name = ?", [token])
+            sqliteConnection.commit()
+            valid = cursor.fetchall()
+            
+            if(len(valid) == 0):
+                return render(request, "403.html", status = 403)
+            
+    except Exception as e:
+            print(e)
+            return render(request, "403.html", status = 403)
 
     if request.method == 'POST':
         map = FormMap(1, request.POST)
